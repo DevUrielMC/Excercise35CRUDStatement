44352dc0932592e94b6de936ccdc0c8670122dc8
DevUrielMC <urimarcervantes.95@gmail.com>
2019-11-26 12:39:43 -0600

    creando el servlet crear producto y añadiendo datos desde java a base de datos en mysql

--- src/com/excercise35crudstatement/controller/CreateServlet.java
+++ src/com/excercise35crudstatement/controller/CreateServlet.java
@@ -6,6 +6,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /***
  * Clase servlet 
@@ -29,7 +36,51 @@ public class CreateServlet extends HttpServlet {
      */
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//response.setContentType("application/json charset='utf-8'");
+		response.setContentType("text/html charset='utf-8'");
+		PrintWriter output = response.getWriter();
+		
+		String nameProduct = request.getParameter("txtNameProduct");
+		double priceProduct = Double.parseDouble(request.getParameter("txtPriceProduct"));
+		
+		//1. Declaramos las variables
+		String urlServidor = "jdbc:mysql://localhost:3306/tiendita?useSSL=false&serverTimezone=UTC";
+		String nombreUsuario = "root";
+		String password = "admin";
+		int rowsAffected=0;
+		
+		//2. Declaramos los objetos
+		/*Connection conn=null;
+		Statement stmnt=null;
+		ResultSet rs=null;
+		*/
+		
+		try {
+			//3. Instanciamos el driver
+			Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+			//4. Abrimos la conexión
+			Connection conn = DriverManager.getConnection(urlServidor, nombreUsuario, password);
+			Statement stmnt = conn.createStatement();
+			
+			//5. Ejecutamos la sentencia sql
+			rowsAffected = stmnt.executeUpdate("INSERT INTO productos (nombreProducto, precioProducto) VALUES ('"+nameProduct+"', "+priceProduct+")");
+			//6. Procesamos los datos
+			if(rowsAffected!=0)
+			{
+				output.append("Registro Añadido con éxito!!");
+			}
+			else
+			{
+				output.append("Registro NO fue añadido!!");
+			}
+			//7. Cerramos los objetos de conexión
+			stmnt.close();
+			conn.close();
+		} catch (SecurityException | ClassNotFoundException | SQLException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+
+		output.close();
 	}
 
 }
